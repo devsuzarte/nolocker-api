73dddda2b1f28fec9a613fa02fdffb61f759d101
BenSuzarte <ben.dantassuzarte@gmail.com>
2025-06-02 00:53:11 -0300

    Setup cors policy

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import fastifyMultipart from "@fastify/multipart"
 import Fastify from "fastify"
+import cors from "@fastify/cors"
 
 const app = Fastify({ logger: true })
 
 // Configs
 app.register(fastifyMultipart)
+app.register(
+    cors,
+    {
+        origin: [
+            "https://nolocker-web-git-main-devsuzartes-projects.vercel.app",
+            "https://nolocker-web-devsuzartes-projects.vercel.app",
+            "https://nolocker-web.vercel.app"
+        ],
+        methods: ['POST']
+    }
+)
 
 export default app
